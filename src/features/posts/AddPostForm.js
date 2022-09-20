8b93fba5e179b3474414f27888892bfddb9331e0
Niraj <amatyaraj@gmail.com>
2022-09-20 23:13:59 +1000

    created controlled form and dispatched action while submitting form

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,16 +1,55 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { postAdded } from './postSlice';
 
 const AddPostForm = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const dispatch = useDispatch();
+
+  // handle title on change
+  const handleTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  // handle Content function
+  const handleContent = (event) => {
+    setContent(event.target.value);
+  };
+
+  // on submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // form will only submit if both the title and content is provided
+    if (title && content) {
+      dispatch(postAdded({ title, content }));
+    }
+    // reset
+    setTitle('');
+    setContent('');
+  };
+
   return (
     <section>
       <h2>Add New Post</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="postTitle">Post Title:</label>
-        <input type="text" id="postTitle" name="postTitle" />
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={handleTitle}
+        />
         <label htmlFor="postContent">Content</label>
-        <textarea name="postContent" id="postContent"></textarea>
+        <textarea
+          name="postContent"
+          id="postContent"
+          value={content}
+          onChange={handleContent}
+        ></textarea>
         <div className="save-post__button">
-          <button type="button">Save Post</button>
+          <button type="submit">Save Post</button>
         </div>
       </form>
     </section>
