ccd9ae977cd04af83694c9f24fb576048f47b698
Niraj <amatyaraj@gmail.com>
2022-09-20 14:30:38 +1000

    Render post lists
    - post lists are recieved from the store are mapped.
    -creating an article for each post.
    -getting id, title and content from the post.
    - to preview only first 100 character substring is used.

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const PostList = () => {
-  return <div>PostList</div>;
+  // getting posts from the store
+  const posts = useSelector((state) => state.posts);
+
+  //   returning posts from the posts state
+  const renderedPosts = posts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+    </article>
+  ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
 };
 
 export default PostList;
