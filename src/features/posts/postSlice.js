88c2975950c85c26c91800bf9c54b88ac7e20769
Niraj <amatyaraj@gmail.com>
2022-09-20 14:00:47 +1000

    created a postSlice with dummy data and exported it

---
+++ src/features/posts/postSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// dummy data
+const initialState = [
+  {
+    id: '1',
+    title: 'Learn to code',
+    content: 'Learn to code everyday.',
+  },
+  {
+    id: '2',
+    title: 'Learn Redux Toolkit',
+    content: 'After you learn React learn Redux Toolkit.',
+  },
+];
+// creating post slice
+const postSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {},
+});
+
+export default postSlice.reducer;
