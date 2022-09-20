d58aa1c10f5e6f9a612e23e33c6a83ea27c5be55
Niraj <amatyaraj@gmail.com>
2022-09-20 23:35:36 +1000

    dummy data for users

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,8 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// dummy data
+const initialState = [
+  { id: '0', name: 'Nirvan Amatya' },
+  { id: '1', name: 'Naisha Amatya' },
+  { id: '2', name: 'Sirjana Amatya' },
+];
+
 const usersSlice = createSlice({
   name: 'users',
-  initialState: '',
+  initialState,
   reducers: {},
 });
 
