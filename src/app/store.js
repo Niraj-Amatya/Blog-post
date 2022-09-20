36f1de5b6c6006b91ab51289445dc203ec204ee9
Niraj <amatyaraj@gmail.com>
2022-09-20 23:31:49 +1000

    Create users slice state
    - Create a user slice store and export userSlice reducer.
    - Import it to the store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postReducer from '../features/posts/postSlice';
+import usersReducer from '../features/users/usersSlice';
 
 const store = configureStore({
-  reducer: { posts: postReducer },
+  reducer: { posts: postReducer, users: usersReducer },
 });
 
 export default store;
