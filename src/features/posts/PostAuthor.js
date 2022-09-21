8d061f923059415cf94b7861a55edbf06afb6c3f
Niraj <amatyaraj@gmail.com>
2022-09-22 09:49:50 +1000

    added some comment for future reference

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 
 const PostAuthor = ({ userId }) => {
   const users = useSelector((state) => state.users);
+  //   finding the author/user who has same userId as saved in the posts.
+  // getting userId as porps from the PostList.
   const author = users.find((user) => user.id === userId);
   return <span> Author: {author ? author.name : 'Unknown'}</span>;
 };
--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectAllPosts } from './postSlice';
+import PostAuthor from './PostAuthor';
 
 const PostList = () => {
   // getting posts from the store
   //   selectAllPosts is a selector from the postSlice
   const posts = useSelector((state) => selectAllPosts(state));
+  console.log(posts);
 
   //   returning posts from the posts state
   const renderedPosts = posts.map((post) => (
@@ -13,6 +15,9 @@ const PostList = () => {
       <h3>{post.title}</h3>
       {/* preview only first 100 characters substring is used */}
       <p>{post.content.substring(0, 100)}</p>
+      <p className="author">
+        <PostAuthor userId={post.userId} />
+      </p>
     </article>
   ));
 
