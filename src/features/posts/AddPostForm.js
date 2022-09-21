5f13c30bf6211170e01c08330f5f65ee55cc530d
Niraj <amatyaraj@gmail.com>
2022-09-21 10:03:21 +1000

    Create select options for Author/users
    - getting users from the store with useSelector.
    - mapping through the users and getting their id and name to use in the select html element to create options for the users
    - Creating Option list in the return and providing the user name and id in the options.

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { postAdded } from './postSlice';
+import { useSelector } from 'react-redux';
 
 const AddPostForm = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [userId, setUserId] = useState('');
+
   const dispatch = useDispatch();
+  const users = useSelector((state) => state.users);
+
+  // get users from store
+  const userOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   // handle title on change
   const handleTitle = (event) => {
@@ -17,12 +28,17 @@ const AddPostForm = () => {
     setContent(event.target.value);
   };
 
+  // state value from the selected options
+  const handleSelect = (event) => {
+    setUserId(event.target.value);
+  };
+
   // on submit
   const handleSubmit = (event) => {
     event.preventDefault();
     // form will only submit if both the title and content is provided
     if (title && content) {
-      dispatch(postAdded({ title, content }));
+      dispatch(postAdded({ title, content, userId }));
     }
     // reset
     setTitle('');
@@ -41,6 +57,15 @@ const AddPostForm = () => {
           value={title}
           onChange={handleTitle}
         />
+        <label htmlFor="postAuthor">Author:</label>
+        <select
+          name="postAuthor"
+          id="postAuthor"
+          value={userId}
+          onChange={handleSelect}
+        >
+          {userOptions}
+        </select>
         <label htmlFor="postContent">Content</label>
         <textarea
           name="postContent"
