07817e00b909d7bbc9db22ea69c9c62b314d4318
Niraj <amatyaraj@gmail.com>
2022-09-21 12:24:03 +1000

    Disable submit button when form field is empty
    - use Boolean method to get true or false value
    - Boolean will be true if title, content or userId has value and false if they are empty/null/undefined.
    - this is used to deciede if form button should be disabled or allowed to submit
    - helper class to select css styling depending on either button is disabled or not.

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -11,6 +11,14 @@ const AddPostForm = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users);
 
+  // Boolean will be true if title, content or userId has value and false if they are empty/null/undefined.
+  // this is used to deciede if form button shoudl be disabled or allowed to submit
+  const canSubmitButton = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  // helperClass for styling of button when canSubmitButton is true or false.
+  // this will be used in submit button
+  const helperClass = canSubmitButton ? 'save-post__button' : 'disabled';
+
   // get users from store
   const userOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
@@ -43,39 +51,48 @@ const AddPostForm = () => {
     // reset
     setTitle('');
     setContent('');
+    setUserId('');
   };
 
   return (
     <section>
       <h2>Add New Post</h2>
+      {!canSubmitButton ? <p>Please fill the form to submit your post</p> : ''}
       <form onSubmit={handleSubmit}>
-        <label htmlFor="postTitle">Post Title:</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          value={title}
-          onChange={handleTitle}
-        />
-        <label htmlFor="postAuthor">Author:</label>
-        <select
-          name="postAuthor"
-          id="postAuthor"
-          value={userId}
-          onChange={handleSelect}
-        >
-          {userOptions}
-        </select>
-        <label htmlFor="postContent">Content</label>
-        <textarea
-          name="postContent"
-          id="postContent"
-          value={content}
-          onChange={handleContent}
-        ></textarea>
-        <div className="save-post__button">
-          <button type="submit">Save Post</button>
+        <div className="form__container">
+          <label htmlFor="postTitle">Post Title:</label>
+          <input
+            type="text"
+            id="postTitle"
+            name="postTitle"
+            value={title}
+            onChange={handleTitle}
+          />
+          <label htmlFor="postAuthor">Author:</label>
+          <select
+            name="postAuthor"
+            id="postAuthor"
+            value={userId}
+            onChange={handleSelect}
+          >
+            <option></option>
+            {userOptions}
+          </select>
+          <label htmlFor="postContent">Content</label>
+          <textarea
+            name="postContent"
+            id="postContent"
+            value={content}
+            onChange={handleContent}
+          ></textarea>
         </div>
+        <button
+          className={helperClass}
+          type="submit"
+          disabled={!canSubmitButton}
+        >
+          Submit Post
+        </button>
       </form>
     </section>
   );
