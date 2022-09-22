d2535c9968789a1f653997e7a92670b3cee2c8a5
Niraj Amatya <AMATYARAJ@GMAIL.COM>
2022-09-22 16:19:23 +1000

    Blog post date  (#3)
    * Install date-fns to show post date and time.
    
    - date-fns is intalled.
    - when post is created in the postSlice new date is also added
    - this is updated in the store
    - date is then mapped in the PostList component to show date in the fron end
    - this date is passed to DatePost component where it is formated accordingly for better user experience.
    
    * sorted the post so that the recent post is always on the top
    
    * code refactor

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -2,14 +2,32 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectAllPosts } from './postSlice';
 import PostAuthor from './PostAuthor';
+import DatePost from './DatePost';
 
 const PostList = () => {
   // getting posts from the store
   //   selectAllPosts is a selector from the postSlice
   const posts = useSelector((state) => selectAllPosts(state));
 
+  // sort the order of the post, so that the recent post is always shown on the top.
+
+  // The localeCompare() method returns a number indicating whether a reference string comes before, or after, or is the same as the given string in sort order.
+  // Returns an integer indicating whether the referenceStr comes before, after or is equivalent to the compareString.
+
+  // Negative when the referenceStr occurs before compareString
+  // Positive when the referenceStr occurs after compareString
+  // Returns 0 if they are equivalent
+
+  // sort method provides a and b to compare the date
+  // slice() creates a shallow copy of the posts without mutating the actual posts array.
+
+  const orderedPost = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+  console.log(orderedPost);
+
   //   returning posts from the posts state
-  const renderedPosts = posts.map((post) => (
+  const renderedPosts = orderedPost.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       {/* preview only first 100 characters substring is used */}
@@ -17,6 +35,9 @@ const PostList = () => {
       <p className="author">
         <PostAuthor userId={post.userId} />
       </p>
+
+      {/* pass the date of the post created from the post store */}
+      <DatePost timestamp={post.date} />
     </article>
   ));
 
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,4 +1,7 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+// for dates in the blog post
+// sub is to subtract desired date from the current date
+import { sub } from 'date-fns';
 
 // dummy data
 const initialState = [
@@ -6,13 +9,23 @@ const initialState = [
     id: '1',
     title: 'Learn to code',
     content: 'Learn to code everyday.',
+    // current date from new Date is subtractet by 2 hours with sub function from date-fns
+    // The toISOString() method returns a string in simplified extended ISO format (ISO 8601), which is always 24 or 27 characters long (YYYY-MM-DDTHH:mm:ss.sssZ or ±YYYYYY-MM-DDTHH:mm:ss.sssZ
+    // look for toISOString() in MDN
+    // new Date() gives new date
+    date: sub(new Date(), {
+      hours: 2,
+    }).toISOString(),
   },
   {
     id: '2',
     title: 'Learn Redux Toolkit',
     content: 'After you learn React learn Redux Toolkit.',
+    // current date from new Date is subtractet by 10 mins with sub function from date-fns
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
   },
 ];
+
 // creating post slice
 const postSlice = createSlice({
   name: 'posts',
@@ -27,9 +40,10 @@ const postSlice = createSlice({
         title: post.title,
         content: post.content,
         userId: post.userId,
+        // new date is created everytime the post is created.
+        // this is updated to the post store and is available store wise
+        date: new Date().toISOString(),
       };
-
-      console.log(newPost);
 
       state.push(newPost);
     },
