3c08837078b8525073bcec88f43c9095b9a2f6c4
Niraj <amatyaraj@gmail.com>
2022-09-24 08:24:39 +1000

    file to save dummy data created and imported to postSlice file

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,51 +1,14 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 // for dates in the blog post
 // sub is to subtract desired date from the current date
-import { sub } from 'date-fns';
+import { initialStateDummyData } from '../../dummyData/dummy';
 
 // dummy data
-const initialState = [
-  {
-    id: '1',
-    title: 'Learn to code',
-    content: 'Learn to code everyday.',
-    // current date from new Date is subtractet by 2 hours with sub function from date-fns
-    // The toISOString() method returns a string in simplified extended ISO format (ISO 8601), which is always 24 or 27 characters long (YYYY-MM-DDTHH:mm:ss.sssZ or ±YYYYYY-MM-DDTHH:mm:ss.sssZ
-    // look for toISOString() in MDN
-    // new Date() gives new date
-    date: sub(new Date(), {
-      hours: 2,
-    }).toISOString(),
-    // reactions for the post
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-
-  {
-    id: '2',
-    title: 'Learn Redux Toolkit',
-    content: 'After you learn React learn Redux Toolkit.',
-    // current date from new Date is subtractet by 10 mins with sub function from date-fns
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-];
 
 // creating post slice
 const postSlice = createSlice({
   name: 'posts',
-  initialState,
+  initialState: initialStateDummyData,
   reducers: {
     // add post reduce
     // recieve payload from the form
@@ -71,6 +34,11 @@ const postSlice = createSlice({
       state.push(newPost);
     },
     // reducer for reactions
+    // will recieve postId and reaction(specifice reaction name) as a dispatch action
+    // destructuring method to get postId and reaction from the payload
+    // checks if it is a new post or an old post in the state array
+    // if new, every reactions starts with value of zero
+    // else specific reaction that is dispatched by clicking that reaction button is added 1
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
       const existingPost = state.find((post) => post.id === postId);
