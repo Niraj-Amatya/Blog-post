c6190e9d62b2c7603eb53a57fb94f3221241093c
Niraj <amatyaraj@gmail.com>
2022-09-26 11:44:23 +1000

    Use createAsyncThunk to generate Users from API
    - thunk is used to fetch users from the API
    - extraReducer is called from create usersSlice
    - on a fullfilled action type, user store gets updated with users.
    
    Call fetchUsers thunk from the index.js
    
    - thunk is called from the store so that it updates the users store as soon as app loads for the first time.
    - this makes sure that PostLists can get access to the users and pass it to its child components and does not need to wait.
    - fetchUsers is dispatched with Store.

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,16 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/users';
 // dummy data
-const initialState = [
-  { id: '0', name: 'Nirvan Amatya' },
-  { id: '1', name: 'Naisha Amatya' },
-  { id: '2', name: 'Sirjana Amatya' },
-];
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await axios.get(POSTS_URL);
+  return response.data;
+});
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
