451c42f11ed34c7d6de1a0f87ef79f11175b29af
Niraj <amatyaraj@gmail.com>
2022-09-26 11:21:28 +1000

    Use createAsyncThunk from reduce toolkit.
    - createAsyncThunk is called for async task like fetching data from the API.
    - createAsyncThunk creates an extra reducer which is passed to createSlice, and called outside the reducers.
    - Different action types like "pending", "fullfilled" and "rejected" which are also promises returned by createAsyncThunk are handled in this extraReducers.

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,67 +1,52 @@
 import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
 import axios from 'axios';
 
-// this is a dummy data, that we used to test the app.
-// import { initialStateDummyData } from '../../dummyData/dummy';//
-
-// fake base url for posts
 const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 const initialState = {
   posts: [],
-  status: 'idle', // "idle" | "loading" | "succeeded" | "failed" |
+  status: 'idle', //'idle' | 'loading' | 'succeeded' | 'failed'
   error: null,
 };
 
-// creating fetchPosts function with createAsyncThunk that takes two argument:
-// 1. action.type which is a sring
-// 2. payload creator callback fucntion which returns promise with data or error
-// used async await inside the callback with try and catch
-// returns data from the base url or error on catch block
-export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-  try {
-    const response = await axios.get(POSTS_URL);
-    return response.data;
-  } catch (error) {
-    return error.message;
-  }
+// fetch post from the API using createAsyncThunk
+// createAsyncThunk will accept two arguments, a string that will be used as a prefix for the generated action types and callback function that will createa a payload and should return promises
+// this is generally written in async and await syntax
+// axios is used to fetch data from the API
+
+export const fetchPosts = createAsyncThunk('posts/fetchAllPosts', async () => {
+  const response = await axios.get(POSTS_URL);
+  return response.data;
 });
 
-// creating post slice
-const postSlice = createSlice({
+const postsSlice = createSlice({
   name: 'posts',
-  // initialState: initialStateDummyData,
   initialState,
   reducers: {
-    // add post reduce
-    // recieve payload from the form
-    postAdded(state, action) {
-      const post = action.payload;
-      const newPost = {
-        id: nanoid(),
-        title: post.title,
-        content: post.content,
-        userId: post.userId,
-        // new date is created everytime the post is created.
-        // this is updated to the post store and is available store wise
-        date: new Date().toISOString(),
-        reactions: {
-          thumbsUp: 0,
-          wow: 0,
-          heart: 0,
-          rocket: 0,
-          coffee: 0,
-        },
-      };
-
-      state.posts.push(newPost);
+    postAdded: {
+      reducer(state, action) {
+        state.posts.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
+          },
+        };
+      },
     },
-    // reducer for reactions
-    // will recieve postId and reaction(specifice reaction name) as a dispatch action
-    // destructuring method to get postId and reaction from the payload
-    // checks if it is a new post or an old post in the state array
-    // if new, every reactions starts with value of zero
-    // else specific reaction that is dispatched by clicking that reaction button is added 1
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
       const existingPost = state.posts.find((post) => post.id === postId);
@@ -70,10 +55,45 @@ const postSlice = createSlice({
       }
     },
   },
+  // extraReducers is a reducer that is called inisde the postSlice, however; outside the reducer, to listen to other action types
+  // it takes one argument: which is a builder
+  // builder is an object type.
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        let min = 1;
+
+        const allLoadedPosts = action.payload.map((post) => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          post.reactions = {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+          };
+
+          return post;
+        });
+
+        state.posts = state.posts.concat(allLoadedPosts);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
 
-export const { postAdded, reactionAdded } = postSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
 
-export default postSlice.reducer;
+export default postsSlice.reducer;
