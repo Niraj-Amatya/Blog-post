73df9c700176459d7ed3899dc0c96dc5a81ac2f0
Niraj <amatyaraj@gmail.com>
2022-10-05 10:21:30 +1100

    Adding routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import PostList from './features/posts/PostList';
 import AddPostForm from './features/posts/AddPostForm';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import PostList from './features/posts/PostList';
 
 function App() {
   return (
-    <main className="App">
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<PostList />} />
+        </Route>
+      </Routes>
+
       <AddPostForm />
       <PostList />
-    </main>
+    </>
   );
 }
 
