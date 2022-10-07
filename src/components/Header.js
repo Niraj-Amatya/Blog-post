537e35f734a674c5a4b0d4db97f4b0fdd5ad7605
Niraj <amatyaraj@gmail.com>
2022-10-07 22:15:51 +1100

    added Navbar with styling

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './header.css';
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <NavLink to="/" className="logo">
+        <h1 className="logo-heading">Blogs</h1>
+      </NavLink>
+
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/post"
+              className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+            >
+              Post
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
