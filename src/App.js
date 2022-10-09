be3588c2b9f4eb5f4a251ca4dd30f896c3c762e9
Niraj <amatyaraj@gmail.com>
2022-10-10 09:10:26 +1100

    added route for EditPost Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddPostForm from './features/posts/AddPostForm';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import SinglePagePost from './features/posts/SinglePagePost';
+import EditPost from './features/posts/EditPost';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/post">
             <Route index element={<AddPostForm />} />
             <Route path=":postId" element={<SinglePagePost />} />
+            <Route path="edit/:postId" element={<EditPost />} />
           </Route>
         </Route>
       </Routes>
