9eb597fd9051c84e99a0a68d33ca11ea75746bb8
Niraj <amatyaraj@gmail.com>
2022-10-14 21:47:22 +1100

    added styling to the title

--- src/features/posts/PostExcerpt.js
+++ src/features/posts/PostExcerpt.js
@@ -9,7 +9,9 @@ import './postExcerpt.css';
 const PostExcerpt = ({ post }) => {
   return (
     <article>
-      <h3>{post.title}</h3>
+      <Link to={`post/${post.id}`} className="more-info-text">
+        <h3 className="post-heading">{post.title.substring(0, 15)}</h3>
+      </Link>
       {/* preview only first 100 characters substring is used */}
       <p>{post.body.substring(0, 70)}</p>
       <p className="author author-flex">
